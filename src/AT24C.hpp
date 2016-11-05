4f376ee8c50d0653e8b1e6c197864cadc683ef3d
Baykush <ata.dvk@gmail.com>
2016-11-05 21:15:58 +0100

    Added documentation.

--- src/AT24C.hpp
+++ src/AT24C.hpp
@@ -1,12 +1,12 @@
-/*
- *  EEPROM AT24C class library
- *      Tested with Teensy + AT24C256B
+/**
  *
- *  Author: Atilla "x41tilla" Özkan
- *  Date: 2016.11.02
+ *   EEPROM AT24C class library
  *
+ *   Tested with Teensy + AT24C256B
+ *
+ *   Author:    Atilla "x41tilla" Özkan
+ *   Date:                   2016-11-02
  */
-
 
 #ifndef AT24C_HPP
 #define	AT24C_HPP
@@ -23,13 +23,46 @@ class AT24C{
 
 public:
 
+    /**
+     * Constructors
+     */
     AT24C();
     AT24C(const uint8_t chipAddress);
 
+
+    /**
+     * Retrieve a single byte from address
+     * @method    read
+     * @param     address    the address to read from
+     * @return               byte read
+     */
     uint8_t read(const ushort address);
+
+    /**
+     * Retrieve a sequence of bytes
+     * @method    read
+     * @param     address    the address to start reading from
+     * @param     buffer     buffer to be filled with retreived data
+     * @param     length     number of bytes to read
+     */
     void read(const ushort address, uint8_t* buffer, const ushort length);
 
+
+    /**
+     * Write a single byte
+     * @method    write
+     * @param     address    the address to write to
+     * @param     data       byte to be written
+     */
     void write(const ushort address, uint8_t data);
+
+    /**
+     * Write a sequence of bytes
+     * @method    write
+     * @param     address    the address to start from
+     * @param     data       data to be written
+     * @param     size       size of the data
+     */
     void write(const ushort address, uint8_t data[], const ushort size);
 
 private:
